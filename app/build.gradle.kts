b3d00d07bcb87e5edd3a13c90f869aaa87d6181e
Georg <georg.m.doll@gmail.com>
2025-02-26 01:02:35 +0100

    first release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "that\'s what we call an MVP"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
